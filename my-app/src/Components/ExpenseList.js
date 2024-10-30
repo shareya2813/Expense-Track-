406655166c2dd473a04342d8f8ffff35e0fb5731
shareya2813 <shareya.sharma@vavensoft.com>
2024-10-30 17:10:37 +0530

    de

--- my-app/src/Components/ExpenseList.js
+++ my-app/src/Components/ExpenseList.js
@@ -1,14 +1,1 @@
-import React from 'react';
-import ExpenseItem from './ExpenseItem';
-
-const ExpenseList = ({ expenses }) => {
-  return (
-    <ul>
-      {expenses.map(expense => (
-        <ExpenseItem key={expense.id} expense={expense} />
-      ))}
-    </ul>
-  );
-};
-
-export default ExpenseList;
+;
