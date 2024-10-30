9d564d9cfadd9a5dab468680d979ca56a6bc8b9b
shareya2813 <shareya.sharma@vavensoft.com>
2024-10-30 17:10:11 +0530

    delete

--- my-app/src/Components/ExpenseItem.js
+++ my-app/src/Components/ExpenseItem.js
@@ -1,11 +1,1 @@
-import React from 'react';
 
-const ExpenseItem = ({ expense }) => {
-  return (
-    <li>
-      {expense.title}: ${expense.amount.toFixed(2)}
-    </li>
-  );
-};
-
-export default ExpenseItem;
